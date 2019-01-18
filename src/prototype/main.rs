ccb7b289b1329968e4c10968198ebd2c1003b46d
Hugh Lang <hughlang@gmail.com>
2019-01-18 12:31:32 -0500

    widgets test

--- src/prototype/main.rs
+++ src/prototype/main.rs
@@ -1,28 +1,105 @@
 extern crate orbtk;
 use orbtk::*;
 
-// extern crate calc;
-// 
-// use std::cell::{Cell, RefCell};
-// use std::rc::Rc;
-// #[derive(Default)]
-// struct MainViewState {
-//     result: RefCell<String>,
-//     updated: Cell<bool>,
-// }
+use std::{cell::Cell, rc::Rc};
+
+#[derive(Default)]
+struct MainViewState {
+    counter: Cell<i32>,
+}
+
+impl MainViewState {
+    pub fn increment(&self) {
+        self.counter.set(self.counter.get() + 1)
+    }
+}
+
+impl State for MainViewState {
+    fn update(&self, context: &mut Context<'_>) {
+        if let Ok(button_count_label) = context.widget().borrow_mut_property::<Label>() {
+            button_count_label.0 = format!("Button count: {}", self.counter.get());
+        }
+    }
+}
+
+fn create_header(text: &str) -> Template {
+    TextBlock::create()
+        .with_property(Label::from(text))
+        .with_property(Selector::from("textblock").with_class("h1"))
+}
+
+fn create_space_row() -> Template {
+    Row::create().with_property(Selector::from("row").with_class("space"))
+}
 
 struct MainView;
 
 impl Widget for MainView {
     fn create() -> Template {
+        let state = Rc::new(MainViewState::default());
+        let button_count_label = SharedProperty::new(Label::from("Button count: 0"));
+
         Template::default()
             .as_parent_type(ParentType::Single)
-            .with_debug_name("MainView")
+            .with_state(state.clone())
             .with_child(
-                Container::create()
-                    .as_parent_type(ParentType::Single)
-                    .with_child(TextBlock::create().with_property(Label::from("OrbTk"))),
+                create_space_row()
+                    .with_child(
+                        Column::create()
+                            .with_child(Container::create().with_child(create_header("Buttons")))
+                            .with_child(
+                                Container::create().with_child(
+                                    Button::create()
+                                        .with_property(Label::from("Button"))
+                                        .with_property(FontIcon::from(
+                                            theme::material_font_icons::CHECK_FONT_ICON,
+                                        ))
+                                        .with_event_handler(MouseEventHandler::default().on_click(
+                                            Rc::new(move |_pos: Point| -> bool {
+                                                state.increment();
+                                                true
+                                            }),
+                                        )),
+                                ),
+                            )
+                            .with_child(
+                                Container::create().with_child(
+                                    Button::create()
+                                        .with_property(
+                                            Selector::from("button").with_class("primary"),
+                                        )
+                                        .with_property(Label::from("Primary")),
+                                ),
+                            )
+                            .with_child(Container::create().with_child(
+                                ToggleButton::create().with_property(Label::from("ToggleButton")),
+                            ))
+                            .with_child(Container::create().with_child(
+                                CheckBox::create().with_property(Label::from("CheckBox")),
+                            ))
+                            .with_child(Container::create().with_child(Switch::create())),
+                    )
+                    .with_child(
+                        Column::create()
+                            .with_child(Container::create().with_child(create_header("Text")))
+                            .with_child(
+                                Container::create().with_child(
+                                    TextBlock::create()
+                                        .with_shared_property(button_count_label.clone())
+                                        .with_property(
+                                            Selector::from("textblock").with_class("fheight"),
+                                        ),
+                                ),
+                            )
+                            .with_child(Container::create().with_child(
+                                TextBox::create().with_property(WaterMark::from("TextBox...")),
+                            )), // .with_child(Container::create().with_child(
+                                //     TextBox::create().with_property(WaterMark::from("TextBox...")),
+                                // ))
+                    ),
             )
+            .with_shared_property(button_count_label)
+            .with_debug_name("MainView")
     }
 }
 
@@ -31,9 +108,9 @@ fn main() {
     application
         .create_window()
         .with_bounds(Bounds::new(0, 0, 420, 730))
-        .with_title("OrbTk - Minimal example")
+        .with_title("OrbTk - Widgets example")
         .with_root(MainView::create())
-        .with_debug_flag(true)
+        .with_debug_flag(false)
         .build();
     application.run();
 }
