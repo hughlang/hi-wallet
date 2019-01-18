73b6bd9c65660e53f0d7f2e731ea331b81005167
Hugh Lang <hughlang@gmail.com>
2019-01-18 18:43:45 -0500

    wallet0 debug

--- src/wallet0/wallet0.rs
+++ src/wallet0/wallet0.rs
@@ -1,6 +1,5 @@
 extern crate orbtk;
 use orbtk::*;
-
 use std::{cell::Cell, rc::Rc};
 
 #[derive(Default)]
@@ -24,7 +23,7 @@ fn create_header(text: &str) -> Template {
     .with_property(Selector::from("textblock").with_class("h1"))
 }
 
-fn create_space_row() -> Template {
+fn make_row() -> Template {
   Row::create().with_property(Selector::from("row").with_class("space"))
 }
 
@@ -33,48 +32,38 @@ struct MainView;
 impl Widget for MainView {
   fn create() -> Template {
     let state = Rc::new(MainViewState::default());
-    let button_count_label = SharedProperty::new(Label::from("Button count: 0"));
+    // let clear_state = state.clone();
+    let label = SharedProperty::new(Label::from("prototype"));
 
     Template::default()
       .as_parent_type(ParentType::Single)
       .with_state(state.clone())
       .with_child(
-        create_space_row().with_child(build_column1()).with_child(
-          Column::create()
+        make_row().with_child(build_column1())
+          .with_child(Column::create()
             .with_child(Container::create().with_child(create_header("Text")))
             .with_child(
               Container::create().with_child(
                 TextBlock::create()
-                  .with_shared_property(button_count_label.clone())
+                  .with_shared_property(label.clone())
                   .with_property(Selector::from("textblock").with_class("fheight")),
               ),
-            )
-            .with_child(
-              Container::create()
-                .with_child(TextBox::create().with_property(WaterMark::from("TextBox..."))),
-            ), // .with_child(Container::create().with_child(
-               //     TextBox::create().with_property(WaterMark::from("TextBox...")),
-               // ))
+            ),
         ),
-      )
-      .with_shared_property(button_count_label)
+			)
+      .with_shared_property(label)
       .with_debug_name("MainView")
   }
+}
+
+// Create a row for buttons and information
+fn _build_navbar() -> Template {
+  Row::create().with_property(Selector::from("row").with_class("space"))	
 }
 
 fn build_column1() -> Template {
   let column = Column::create()
     .with_child(Container::create().with_child(create_header("Buttons")))
-    .with_child(
-      Container::create().with_child(
-        Button::create()
-          .with_property(Label::from("Button"))
-          .with_property(FontIcon::from(theme::material_font_icons::CHECK_FONT_ICON))
-          .with_event_handler(
-            MouseEventHandler::default().on_click(Rc::new(move |_pos: Point| -> bool { true })),
-          ),
-      ),
-    )
     .with_child(
       Container::create().with_child(
         Button::create()
@@ -95,12 +84,14 @@ fn build_column1() -> Template {
 
 fn main() {
   let mut application = Application::default();
+  // let theme = format!("{}{}", LIGHT_THEME_EXTENSION, LIGHT_THEME_EXTENSION_CSS);
   application
     .create_window()
-    .with_bounds(Bounds::new(0, 0, 420, 730))
-    .with_title("OrbTk - Widgets example")
+    .with_bounds(Bounds::new(50, 50, 600, 400))
+    .with_title("Hi Wallet :: wallet0")
+    // .with_theme(Theme::parse(&theme))
     .with_root(MainView::create())
-    .with_debug_flag(false)
+    .with_debug_flag(true)
     .build();
   application.run();
 }
