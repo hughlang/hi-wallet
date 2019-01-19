27cb998a1613b39953fd8ce56848e9a148e6b619
Hugh Lang <hughlang@gmail.com>
2019-01-19 10:17:03 -0500

    navbar started

--- src/wallet0/wallet0.rs
+++ src/wallet0/wallet0.rs
@@ -17,14 +17,10 @@ impl State for MainViewState {
   }
 }
 
-fn create_header(text: &str) -> Template {
+fn _create_header(text: &str) -> Template {
   TextBlock::create()
     .with_property(Label::from(text))
     .with_property(Selector::from("textblock").with_class("h1"))
-}
-
-fn make_row() -> Template {
-  Row::create().with_property(Selector::from("row").with_class("space"))
 }
 
 struct MainView;
@@ -38,48 +34,22 @@ impl Widget for MainView {
     Template::default()
       .as_parent_type(ParentType::Single)
       .with_state(state.clone())
-      .with_child(
-        make_row().with_child(build_column1())
-          .with_child(Column::create()
-            .with_child(Container::create().with_child(create_header("Text")))
-            .with_child(
-              Container::create().with_child(
-                TextBlock::create()
-                  .with_shared_property(label.clone())
-                  .with_property(Selector::from("textblock").with_class("fheight")),
-              ),
-            ),
-        ),
-			)
+      .with_child(build_navbar()) // Nav bar
       .with_shared_property(label)
       .with_debug_name("MainView")
   }
 }
 
 // Create a row for buttons and information
-fn _build_navbar() -> Template {
-  Row::create().with_property(Selector::from("row").with_class("space"))	
-}
-
-fn build_column1() -> Template {
-  let column = Column::create()
-    .with_child(Container::create().with_child(create_header("Buttons")))
-    .with_child(
-      Container::create().with_child(
-        Button::create()
-          .with_property(Selector::from("button").with_class("primary"))
-          .with_property(Label::from("Primary")),
-      ),
-    )
-    .with_child(
-      Container::create()
-        .with_child(ToggleButton::create().with_property(Label::from("ToggleButton"))),
-    )
-    .with_child(
-      Container::create().with_child(CheckBox::create().with_property(Label::from("CheckBox"))),
-    )
-    .with_child(Container::create().with_child(Switch::create()));
-  column
+fn build_navbar() -> Template {
+  Row::create().with_property(Selector::from("row").with_class("space"))
+	.with_child(
+		Column::create().with_child(
+			Button::create()
+	      .with_property(Selector::from("button").with_class("primary"))
+	      .with_property(Label::from("Primary")),			
+		)
+	)
 }
 
 fn main() {
